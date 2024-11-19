3a5c4aff6c0d6efdcdbe33e4556eb5f90e951ddf
jay <jaceyszhang@outlook.com>
2024-11-19 23:36:39 +0100

    je kan een paar workouts loggen en als t goed is gaat deze ook een db in (+ heb wat opmerkingen bij andere pagina's geplaatst puur voor overzicht voor mezelf)

---
+++ BuddyFitProject/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BuddyFitProject.Models;
+
+public class Workout
+{
+    public string Id { get; set; } 
+    public string Pushups { get; set; }
+    public string Situps { get; set; }
+    public string Plank { get; set; } 
+    public string JumpingJacks { get; set; }
+    public string Running { get; set; } 
+    public string Squats { get;set; }
+
+    public DateTime LoggedAt { get; set; } = DateTime.Now;
+
+}
