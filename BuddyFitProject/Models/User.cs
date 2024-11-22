6e25f584b783189b3f8d3e425ed53eb21a8d54ba
LiesbethHemmers <liesbeth.hemmers@gmail.com>
2024-11-22 17:41:13 +0100

    Je kan kan nu workouts loggen, en deze worden opgeslagen in een tabel.

--- BuddyFitProject/Models/User.cs
+++ BuddyFitProject/Models/User.cs
@@ -9,4 +9,6 @@ public class User
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
+    //public int Coins { get; set; } = 0;
+
 }
