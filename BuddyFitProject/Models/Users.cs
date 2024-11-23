197a4a0b171066c83347fb261b200b7cfe54185b
jay <jaceyszhang@outlook.com>
2024-11-23 22:27:09 +0100

    heb geprobeerd de verschillende versies beetje samen te voegen en db werkende te houden

--- BuddyFitProject/Models/Users.cs
+++ BuddyFitProject/Models/Users.cs
@@ -6,15 +6,16 @@ namespace BuddyFitProject.Models;
 public class Users
 {
     [Key]
-    public string UserName { get; set; }
+    public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
     //public int Coins { get; set; } = 0;
-
+    /*
     public Users()
     {
         Age = 0;
         Start_condition = 0;
         Coins = 0;
     }
+    */
 }
