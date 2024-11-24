46f21c63a2affe77586dec7935d2375554f1f112
jay <jaceyszhang@outlook.com>
2024-11-24 16:28:28 +0100

    verder geklooi met db + heb geprobeerd om db te weergeven in een tabel (niet gelukt :()

--- BuddyFitProject/Models/Users.cs
+++ BuddyFitProject/Models/Users.cs
@@ -6,9 +6,11 @@ namespace BuddyFitProject.Models;
 public class Users
 {
     [Key]
+    public int UserId {get;set;}
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
+
     //public int Coins { get; set; } = 0;
     /*
     public Users()
