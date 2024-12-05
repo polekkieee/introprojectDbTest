0b4b4d2aeec916bacbbac1e442931efd82a369ce
LiesbethHemmers <liesbeth.hemmers@gmail.com>
2024-12-05 11:50:10 +0100

    Voor de zekerheid

--- BuddyFitProject/Data/Models/UserAndPet.cs
+++ BuddyFitProject/Data/Models/UserAndPet.cs
@@ -2,7 +2,10 @@
 {
     public class UserAndPet
     {
-		public Users User { get; set; } = new Users();
-		public Pets Pet { get; set; } = new Pets();
+        public Users User { get; set; } = new Users();
+        public Pets Pet { get; set; } = new Pets();
+        public bool cat { get; set; } = false;
+        public bool bear { get; set; } = false;
+        public bool bunny { get; set; } = false;
     }
 }
