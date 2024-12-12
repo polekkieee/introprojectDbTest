0c696e8f973f8de86a22137857aa9ae5542902e2
LiesbethHemmers <liesbeth.hemmers@gmail.com>
2024-12-12 23:33:53 +0100

    nog wat gecomment bij userinfo

--- BuddyFitProject/Components/Services/UserService.cs
+++ BuddyFitProject/Components/Services/UserService.cs
@@ -16,12 +16,12 @@ namespace BuddyFitProject.Components.Services
     {
         private IDbContextFactory<BuddyFitDbContext> DbContextFactory;
 
-        public UserService(IDbContextFactory<BuddyFitDbContext> dbContext)
+        public UserService(IDbContextFactory<BuddyFitDbContext> dbContext) //Constructor
         {
             DbContextFactory = dbContext;
         }
 
-        public void AddUser(Users user)
+        public void AddUser(Users user) //Saves the user to the database
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -30,7 +30,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public Users GetUserByName(string name)
+        public Users GetUserByName(string name) //Retrieves the user from the database using their uername
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -39,7 +39,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public Users GetUserById(int id)
+        public Users GetUserById(int id) //Retrieves the user from the database using their id
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -48,7 +48,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public Users GetUserByLogin(string name, string password)
+        public Users GetUserByLogin(string name, string password) //Retrieves the user from the database using their username and password
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -57,7 +57,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public Users DeleteUser(Users user)
+        public Users DeleteUser(Users user) //Logic to delete the user
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -68,14 +68,14 @@ namespace BuddyFitProject.Components.Services
                     throw new InvalidOperationException("User not found in the database.");
                 }
 
-                dbContext.Users.Remove(userToDelete);
+                dbContext.Users.Remove(userToDelete); //Remove the user from the database
                 dbContext.SaveChanges();
 
                 return userToDelete;
             }
         }
 
-        public void UpdateUser(Users user)
+        public void UpdateUser(Users user) //Updating user in the database
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -84,7 +84,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public bool ValidateUser(string username, string password)
+        public bool ValidateUser(string username, string password) //Validates user by username and password
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -92,7 +92,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public bool ValidateUserByEmailAndUsername(string username, string email)
+        public bool ValidateUserByEmailAndUsername(string username, string email) //Validates user by username and email
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -100,7 +100,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public bool ValidateUserByEmaiUsernameAndPassword(string username, string email, string password)
+        public bool ValidateUserByEmaiUsernameAndPassword(string username, string email, string password) //Validates user by username, email and password
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -108,15 +108,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public bool ValidateUserByEmail(string email)
-        {
-            using (var dbContext = this.DbContextFactory.CreateDbContext())
-            {
-                return dbContext.Users.SingleOrDefault<Users>(x => x.Email == email) != null;
-            }
-        }
-
-        public Users GetUserByUsernameAndEmail(string username, string email)
+        public Users GetUserByUsernameAndEmail(string username, string email) //Retrieves the user from the database using their username and email
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -125,23 +117,14 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public Users GetUserByEmail(string email)
-        {
-            using (var dbContext = this.DbContextFactory.CreateDbContext())
-            {
-                return dbContext.Users.SingleOrDefault<Users>(x => x.Email == email);
-
-            }
-        }
-
-        public bool NewUser(string username, string startcondition)
+        public bool NewUser(string username, string startcondition) //Checks if the user is new
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
                 return dbContext.Users.SingleOrDefault(x => x.Username == username && x.Start_condition == "new") != null;
             }
         }
-        public void AddWorkout(Users user, WorkoutSessions workout)
+        public void AddWorkout(Users user, WorkoutSessions workout) //Saves workout to the database
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -152,7 +135,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public bool UserExists(string username, string email)
+        public bool UserExists(string username, string email) //Checks if the user exist
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
@@ -160,7 +143,7 @@ namespace BuddyFitProject.Components.Services
             }
         }
 
-        public void AddPet(Pets pet)
+        public void AddPet(Pets pet) //Saves the pet's information into the database
         {
             using (var dbContext = this.DbContextFactory.CreateDbContext())
             {
