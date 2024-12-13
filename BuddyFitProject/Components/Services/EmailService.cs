52bdc8477fcf69f306bce4a1b48b0b07d656febb
LiesbethHemmers <liesbeth.hemmers@gmail.com>
2024-12-13 11:28:16 +0100

    realiseer me nu pas dat register ook beter onder account kan en de html bij logout kon ook nog weg

--- BuddyFitProject/Components/Services/EmailService.cs
+++ BuddyFitProject/Components/Services/EmailService.cs
@@ -19,7 +19,7 @@ namespace BuddyFitProject.Components.Services
         {
             var client = new SendGridClient(_configuration["SendGrid:ApiKey"]); //Creates a sendgrid client
             var from = new EmailAddress("e.s.hemmers@students.uu.nl", "Liesbeth");
-            var to = new EmailAddress(toEmail, "Dear BuddyFitter");
+            var to = new EmailAddress(toEmail);
             var plainTextContent = $"{resetcode}"; //Defines the plaintext content of the email 
             var htmlContent = $"<strong>{resetcode}</strong>"; //Defines the HTML content
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent); //Creates the email message
